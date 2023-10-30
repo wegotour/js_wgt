9607cbf9e75d53afd14e28c1934ca372de4d888e
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-10-30 10:20:44 +0700

    update (js) postfunc.js

--- postfunc.js
+++ postfunc.js
@@ -7,7 +7,7 @@ export default function PostSignUp() {
     "https://asia-southeast2-fahad-402509.cloudfunctions.net/gcfproyekdp";
   let tokenkey = "token";
   let tokenvalue =
-    "56d8745202edebd463b054fdcf224fc552606b36f5b89e36b501ccdd890d902075127f0784fe57e03700c6a1ba46bcf304d912e77f87cc559835503e603a4347";
+    "75127f0784fe57e03700c6a1ba46bcf304d912e77f87cc559835503e603a4347";
   let datainjson = {
     username: getValue("username"),
     password: getValue("password"),
