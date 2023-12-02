9380dc01908d2a7bb86e917a7cf6833eabe628c3
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-12-02 18:12:10 +0700

    update dulu template js nya lekk

--- js/template/template.js
+++ js/template/template.js
@@ -6,4 +6,7 @@ export let URLPost = `https://asia-southeast2-wegotour-403712.cloudfunctions.net
 //url login 
 export let URLLogin = `https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotourlogin`
 
+//url changepass
+export let URLChgPass = 'https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotourchangepassworduser'
+
 export let token = 'token';
