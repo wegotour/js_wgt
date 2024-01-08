a4700a8eb867751dafd6527e9d8e3ed2cd263f5d
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-08 18:29:00 +0700

    update js user

--- js/config/config.js
+++ js/config/config.js
@@ -63,10 +63,18 @@ function ResponsePostLogin(response) {
         console.log('Token User:', response.token);
         setCookieWithExpireHour('user_token', response.token, 2);
         window.location.href = 'https://wegotour.my.id/dashboard/';
-        alert("Selamat Datang")
-    } else {
-        alert('Login gagal. Silakan coba lagi.');
-    }
+        Swal.fire({
+            icon: 'success',
+            title: 'Login Successful',
+            text: 'You have successfully logged in!',
+          });
+} else {
+  Swal.fire({
+            icon: 'error',
+            title: 'Login Failed',
+            text: 'Invalid email or password. Please try again.',
+          });
+}
 }
 
 
