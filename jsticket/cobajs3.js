f90ef72a6c0d2f0dccfb150825f8b0caf93df805
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-17 01:10:59 +0700

    update js

--- jsticket/cobajs3.js
+++ jsticket/cobajs3.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Fetch the daily quote from the API
-    fetch('https://api.lolhuman.xyz/api/random/katabijak?apikey=bijikuda')
-        .then(response => response.json())
-        .then(data => {
-            // Update the content of the paragraph with the daily quote quote hari ini
-            document.getElementById('daily-quote').textContent = `kata - kata hari ini : ${data.result}`;
-        })
-        .catch(error => console.error('Error fetching daily quote:', error));
-});
     // Menghilangkan animasi loading dan menampilkan konten utama setelah 3 detik
     setTimeout(function () {
         document.getElementById('loader-wrapper').style.display = 'none';
