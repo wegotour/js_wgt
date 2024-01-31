e2fd6c92bf5524f132e59a7eb3316616d37cdbb3
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-02-01 01:31:44 +0700

    add nomorid

--- jsticket/cobajs4.js
+++ jsticket/cobajs4.js
@@ -2,6 +2,7 @@
   var selectedTicket = JSON.parse(localStorage.getItem('selectedTicket'));
 
   // Display ticket information on the checkout page
+  document.getElementById('nomorid').value = selectedTicket.nomorid;
   document.getElementById('title').value = selectedTicket.title;
   document.getElementById('description').value = selectedTicket.description;
   document.getElementById('image').value = selectedTicket.image;
