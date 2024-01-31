ae97f1fbe6173099384625b34bd2b768aa2b54e6
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-31 21:51:20 +0700

    update url gcf

--- js/template/template.js
+++ js/template/template.js
@@ -1,12 +1,12 @@
 // template js
 
 //url register
-export let URLPost = `https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotour-registeruser`
+export let URLPost = `https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotour-registersuperadmin`
 
 //url login 
-export let URLLogin = `https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotourlogin`
+export let URLLogin = `https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotourloginadminbaru`
 
 //url changepass
-export let URLChgPass = 'https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotourchangepassworduser'
+// export let URLChgPass = 'https://asia-southeast2-wegotour-403712.cloudfunctions.net/wegotourchangepassworduser'
 
 export let token = 'token';
